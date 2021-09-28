7f15be4d869504d62b4516b25b1bdf288f6a8cf9
stuartErnstsen <stuarternstsen@gmail.com>
2021-09-28 11:55:45 -0600

    added rollbar to server

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express')
+const { TooManyRequests } = require('http-errors')
 const path = require('path')
 const app = express()
 
+const Rollbar = require('rollbar')
+
+const rollbar = new Rollbar({
+    accessToken: '0cc1e6fa15594d0296ec5cd3f69d8ead',
+    captureUncaught: true,
+    captureUnhandledRejections: true
+})
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './client/index.html'))
+    rollbar.info('Html was monitored successfully!')
 })
 
 const port = process.env.PORT || 5656
+
+app.use(rollbar.errorHandler())
 
 app.listen(port, () => console.log(`Hippity Hoppity your server is poppening on port: ${port}`))
