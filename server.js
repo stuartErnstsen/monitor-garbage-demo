55ac287c86771d9c867c371e169afeb35c23ef8a
stuartErnstsen <stuarternstsen@gmail.com>
2021-09-28 12:31:54 -0600

    add json body parser

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+app.use(express.json())
+
 const Rollbar = require('rollbar')
 
 const rollbar = new Rollbar({
